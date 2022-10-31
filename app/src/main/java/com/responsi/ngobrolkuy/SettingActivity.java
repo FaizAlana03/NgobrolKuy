a91efe319f78211cfd81932562e5b2a86a409515
FaizAlana03 <faizalana6@gmail.com>
2022-10-31 12:04:43 +0700

    Commit a fellony

--- app/src/main/java/com/responsi/ngobrolkuy/SettingActivity.java
+++ app/src/main/java/com/responsi/ngobrolkuy/SettingActivity.java
@@ -2,16 +2,13 @@ package com.responsi.ngobrolkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-public class SplashActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_setting);
     }
 }
